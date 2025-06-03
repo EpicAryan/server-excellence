0a9161c062e4ef4e2354f5f9e507d2be3c2e24e4
EpicAryan <adaryan0811@gmail.com>
2025-06-04 04:21:54 +0530

    fix: dotenv import

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-
+import dotenv from 'dotenv';
+dotenv.config();
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
